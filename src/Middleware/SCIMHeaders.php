cddaaad7508794d86a6f44c0926ce75fb145e35a
Brady Wetherington <bwetherington@grokability.com>
2022-11-07 17:10:16 +0000

    Initial stab at middelware that reformats SCIM responses

--- src/Middleware/SCIMHeaders.php
+++ src/Middleware/SCIMHeaders.php
@@ -6,6 +6,23 @@ use Closure;
 use Illuminate\Http\Request;
 use ArieTimmerman\Laravel\SCIMServer\Exceptions\SCIMException;
 
+function undefault_schema(\stdClass $parsed_json)
+{
+    foreach($parsed_json AS $key => $value) {
+        if (stristr($key,'urn:ietf:params:scim:schemas:core:') !== false) {
+            \Log::error("Found the schema key! It's: $key");
+            unset($parsed_json->{$key}); //yank it out
+            foreach($value AS $subkey => $subval) { //iterate through *its* subkey/subvals...
+                // TODO should we check if the original keys exist? ONly overwrite them if they don't?
+                $parsed_json->{$subkey} = $subval;
+            }
+        } else {
+            \Log::error("didn't find schema key in $key");
+        }
+    }
+    return $parsed_json; // FIXME - actually, uh, do the thing?
+}
+
 class SCIMHeaders
 {
     public function handle(Request $request, Closure $next)
@@ -15,6 +32,23 @@ class SCIMHeaders
         }
         
         $response = $next($request);
+        \Log::error("Response is: ".print_r($response->content(),true));
+        $response_content = json_decode($response->content());
+
+        if ( ! $response_content->totalResults) {
+            \Log::error("doing regular response parsing");
+            $response->setContent(json_encode(undefault_schema($response_content)));
+        } else {
+            \Log::error("doing array-ish work on response...");
+            $final_response = [];
+            foreach($response_content->Resources AS $index => $object) {
+                $final_response[] = undefault_schema($object);
+            }
+            $response_content->Resources = $final_response;
+            $response->setContent(json_encode($response_content));
+//        } else {
+//            \Log::error("UNKNOWN SCHEMA TYPE!!!! What's going on here?");
+        }
         
         return $response->header('Content-Type', 'application/scim+json');
     }
